3ef31c6daacb87d8c9a4484a9c060f49ac142a65
Дмитрий Терешкин <alexrumling2000@gmail.com>
2024-11-24 20:46:59 +0300

    init

---
+++ app/src/main/java/com/surendramaran/yolov8tflite/Constants.kt
@@ -0,0 +1,6 @@
+package com.surendramaran.yolov8tflite
+
+object Constants {
+    const val MODEL_PATH = "best_float32.tflite"
+    val LABELS_PATH: String? = null // provide your labels.txt file if the metadata not present in the model
+}
